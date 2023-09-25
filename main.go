44f203c803e6cc548d6410304927a8232b68e54d
Thameera Senanayaka <me@thameera.com>
2023-09-25 21:12:48 +1000

    Write pem to file with -w flag

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/base64"
 	"encoding/pem"
+	"flag"
 	"fmt"
 	"os"
 
@@ -26,13 +27,27 @@ func certToPEM(cert string) string {
 	return pemData.String()
 }
 
+func writeCertToFile(filename, cert string) {
+	if err := os.WriteFile(filename, []byte(cert), 0644) ; err != nil {
+		fmt.Println("Error writing file")
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf("Wrote certificate to %s\n", filename)
+}
+
 func main() {
-	if len(os.Args) < 2 || len(os.Args) > 2 {
+	var outFileName string
+	flag.StringVar(&outFileName, "w", "", "Output file name")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) != 1 {
 		fmt.Println("Usage: samlmeta <filename>")
-		return
+		os.Exit(1)
 	}
 
-	filename := os.Args[1]
+	filename := args[0]
 	f, err := os.Open(filename)
 	if err != nil {
 		fmt.Println(err)
@@ -46,13 +61,17 @@ func main() {
 	}
 
 	// Get the sign-in certificate
-	cert := xmlquery.FindOne(doc, "//*[local-name(.)='EntityDescriptor']/*[local-name(.)='IDPSSODescriptor']/*[local-name(.)='KeyDescriptor' and @use='signing']/*[local-name(.)='KeyInfo' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']/*[local-name(.)='X509Data']/*[local-name(.)='X509Certificate']/text()")
-	fmt.Printf("Sign-in Certificate (PEM):\n%s\n", certToPEM(cert.Data))
+	certData := xmlquery.FindOne(doc, "//*[local-name(.)='EntityDescriptor']/*[local-name(.)='IDPSSODescriptor']/*[local-name(.)='KeyDescriptor' and @use='signing']/*[local-name(.)='KeyInfo' and namespace-uri(.)='http://www.w3.org/2000/09/xmldsig#']/*[local-name(.)='X509Data']/*[local-name(.)='X509Certificate']/text()")
+	cert := certToPEM(certData.Data)
+	fmt.Printf("Sign-in Certificate (PEM):\n%s\n", cert)
 
 	// Get the Sign-in URL
 	signin := xmlquery.FindOne(doc, "//SingleSignOnService[@Binding='urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST']")
 	if signin != nil {
-		fmt.Printf("Sign-in URL:\n%s", signin.SelectAttr("Location"))
+		fmt.Printf("Sign-in URL:\n%s\n", signin.SelectAttr("Location"))
 	}
 
+	if outFileName != "" {
+		writeCertToFile(outFileName, cert)
+	}
 }
